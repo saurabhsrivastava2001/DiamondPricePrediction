66bc5798f60013ba403818c1041e625a6ed212f4
saurabh srivastava <srisaurabh2001@gmail.com>
2023-11-02 21:06:01 +0530

    personel info

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import find_packages,setup
 setup(
     name='DimondPricePrediction',
     version='0.0.1',
-    author='sunny savita',
-    author_email='sunny.savita@ineuron.ai',
+    author='Saurabh Srivastava',
+    author_email='srisaurabh@gmail.com',
     install_requires=["scikit-learn","pandas","numpy"],
     packages=find_packages()
 )
